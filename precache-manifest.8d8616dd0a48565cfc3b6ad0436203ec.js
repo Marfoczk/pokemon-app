572b7cfd9d24f29f4f5395dd1f1f9a8ee835a265
Marcin Głażewski <marfoczok@gmail.com>
2020-09-06 15:21:47 +0200

    Updates

---
+++ precache-manifest.8d8616dd0a48565cfc3b6ad0436203ec.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e31357809c3ec2e7a4fd6615106ef7",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "bec44d52b89afde219bc",
+    "url": "/pokemon-app/static/css/2.39d2de15.chunk.css"
+  },
+  {
+    "revision": "cff642b1d269ade4e83e",
+    "url": "/pokemon-app/static/css/main.f505aff6.chunk.css"
+  },
+  {
+    "revision": "bec44d52b89afde219bc",
+    "url": "/pokemon-app/static/js/2.4727c79c.chunk.js"
+  },
+  {
+    "revision": "775071e7908c30b629946039840806b0",
+    "url": "/pokemon-app/static/js/2.4727c79c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff642b1d269ade4e83e",
+    "url": "/pokemon-app/static/js/main.bbcd5c40.chunk.js"
+  },
+  {
+    "revision": "401f1db01ac3c1923dd0",
+    "url": "/pokemon-app/static/js/runtime-main.5bd63edd.js"
+  },
+  {
+    "revision": "3cfd5e85cd2597eb00d7f5b0139d3b6d",
+    "url": "/pokemon-app/static/media/logo.3cfd5e85.svg"
+  },
+  {
+    "revision": "bef840e85d7d29cd7efe0bf9857479f3",
+    "url": "/pokemon-app/static/media/no-photos.bef840e8.svg"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/pokemon-app/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/pokemon-app/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/pokemon-app/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/pokemon-app/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/pokemon-app/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/pokemon-app/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/pokemon-app/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/pokemon-app/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/pokemon-app/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/pokemon-app/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/pokemon-app/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/pokemon-app/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/pokemon-app/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/pokemon-app/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/pokemon-app/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/pokemon-app/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
